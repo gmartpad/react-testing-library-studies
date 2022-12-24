399331d977420613ac8aaa1c39f64f8a5b81c350
Gabriel MP <contato.gabrielmartinspadoin@gmail.com>
2022-12-24 03:22:20 -0300

    [WEB] before-after-each - feat: implemented beforeAll, beforeEach, afterEach and afterAll methods in tests

--- src/components/FollowersList/__test__/FollowersList.test.js
+++ src/components/FollowersList/__test__/FollowersList.test.js
@@ -11,11 +11,29 @@ const MockFollowersList = () => {
 }
 
 describe("FollowersList", () => {
+
+  beforeAll(() => {
+    console.log('Running once before all tests in the FollowersList list of tests')
+  })
+
+  beforeEach(() => {
+    console.log('Running before each test in the FollowersList list of tests')
+  })
+
   it('should render follower item', async () => {
     render(<MockFollowersList/>)
     const followerDivElement = await screen.findByTestId("follower-item-0")
     expect(followerDivElement).toBeInTheDocument()
   })
+
+  afterEach(() => {
+    console.log('Running after each test in the FollowersList list of tests')
+  })
+
+  afterAll(() => {
+    console.log('Running once after all tests in the FollowersList list of tests')
+  })
+
   // it('should render multiple followers items', async () => {
   //   render(<MockFollowersList/>)
   //   const followerDivElements = await screen.findAllByTestId(/follower-item/i)
